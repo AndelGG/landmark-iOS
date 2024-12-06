52e04fb858266ffeb6817355461315392dda8185
andelgg <andolanderfgh@gmail.com>
2024-12-06 16:19:20 +0300

    Initial Commit

---
+++ PyHub/PyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyHubApp.swift
+//  PyHub
+//
+//  Created by Дмитрий Поповский on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct PyHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
