99c34253251d8fd6c83a2b883aa0cb34aebe1c83
andelgg <andolanderfgh@gmail.com>
2024-12-06 17:11:09 +0300

    make swift app

--- PyHub/ContentView.swift
+++ PyHub/ContentView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Rectangle().frame(width: 500, height: 1000)
+                Circle().fill(Color.white)
+                VStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                    Button("Go to GitHub") {
+                        openGitHub()
+                    }
+                }
+                
+            }
         }
         .padding()
+    }
+    
+    func openGitHub() {
+        if let url = URL(string: "https://github.com") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
 }
 
