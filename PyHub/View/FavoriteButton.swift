4adc478cb5b2d2d7db11a6b8d55a2276b48b95bf
Popovsky Dmitry <andolanderfgh@gmail.com>
2025-01-29 23:36:30 +0300

    created favorite button for landmark. added filter

---
+++ PyHub/View/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  PyHub
+//
+//  Created by Дмитрий Поповский on 2025-01-29.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
